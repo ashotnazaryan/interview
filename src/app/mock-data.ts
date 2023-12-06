a33792d16f706e399aee0deff27b8802662d4d8a
anazaryan <ashotnazaryan45@gmail.com>
2023-12-06 10:34:07 +0100

    init

---
+++ src/app/mock-data.ts
@@ -0,0 +1,42 @@
+export const PRODUCTS = [
+  {
+    id: '1',
+    title: 'Product 1',
+    type: 'old'
+  },
+  {
+    id: '2',
+    title: 'Product 2',
+    type: 'old'
+  },
+  {
+    id: '3',
+    title: 'Product 3',
+    type: 'new'
+  },
+  {
+    id: '4',
+    title: 'Product 4',
+    type: 'old'
+  },
+  {
+    id: '5',
+    title: 'Product 5',
+    type: 'old'
+  }
+];
+
+export const USERS = [
+  {
+    id: '1',
+    title: 'Name 1'
+  },
+  {
+    id: '2',
+    title: 'Name 2'
+  },
+  {
+    id: '3',
+    title: 'Name 3'
+  }
+];
